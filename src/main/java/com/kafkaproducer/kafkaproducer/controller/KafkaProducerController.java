cdd0d11e3334a161071b3b2891aca3f7b562675e
Naveen Kumar Venkatachalam <msaznaveen@outlook.com>
2020-05-10 08:22:00 +0530

    initial commit for kafka producer(JSON)

---
+++ src/main/java/com/kafkaproducer/kafkaproducer/controller/KafkaProducerController.java
@@ -0,0 +1,28 @@
+package com.kafkaproducer.kafkaproducer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kafkaproducer.kafkaproducer.entity.UserDetails;
+
+@RestController
+public class KafkaProducerController {
+	@Autowired
+	KafkaTemplate<String, Object> kafkatemplate;
+	final String TOPIC = "kafkaMessageBroker001";
+	
+	@GetMapping("/msg")
+	public String kafkaMessage() {
+		System.out.println("TOPIC" + TOPIC);
+		kafkatemplate.send(TOPIC, new UserDetails(1,"naveen","naveenvenkat@gmail.com"));
+		return "Message PublishedSuccessfully";
+	}
+
+	@GetMapping("/get")
+	public String sampleGet() {
+
+		return "Sample geT";
+	}
+}
